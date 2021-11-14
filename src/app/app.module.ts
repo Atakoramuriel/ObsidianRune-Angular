1452758848987eefb0b7e66e346528a425afe96d
Lord muriel <lordmuriel@mbp14inch2021.myfiosgateway.com>
2021-11-14 14:54:10 -0500

    begin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBT9KgVgHpDyJt_-11MNEKo_2wJb8jnqfg",
+    authDomain: "obsidianrune-vuejs.firebaseapp.com",
+    databaseURL: "https://obsidianrune-vuejs.firebaseio.com",
+    projectId: "obsidianrune-vuejs",
+    storageBucket: "obsidianrune-vuejs.appspot.com",
+    messagingSenderId: "588393651282",
+    appId: "1:588393651282:web:84be2d26360266176f538b"
+  }
 };
 
 /*
