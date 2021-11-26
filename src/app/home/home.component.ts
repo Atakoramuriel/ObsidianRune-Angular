3415b486acbb03ae1b1399781e6253f5781326a6
Lord muriel <lordmuriel@mbp14inch2021.myfiosgateway.com>
2021-11-26 16:03:01 -0500

    update to the dashboard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -60,48 +60,145 @@ export class HomeComponent implements OnInit {
   ];
   WritingList = [
     {
-      title: 'Solas Rojo',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16"
+      title: 'Thank you',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      sample: `Thankful I am thankful for love 
+      I am thankful for trust 
+      I am thankful for a day 
+      Where I can lust 
+      Not myself 
+      But my ambition 
+      For there is a honest intention 
+      In making great decisions. 
+      I am thankful for my mom 
+      Who has shown me up 
+      And has shown me down 
+      For I have grown 
+      Into the women I am now 
+      I am thankful for my siblings 
+      For they gave me 
+      A Reason to live 
+      When I didn’t see it give  
+      Me a place to forgive
+      I am thankful for you 
+      For you are great 
+      You are a smile 
+      On a gloomy day 
+      You show me love 
+      You show me trust 
+      You show me things 
+      That I should forever lust. 
+      Thank you. `
+      
+      
     },
     {
-      title: 'Dia de Nocturne',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16"
+      title: 'Breathless',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      sample: `
+              The sun has sunken 
+            The moon has woken 
+        A kiss; the body is drunken 
+        A touch; it gives the unspoken 
+        They both know the function 
+        As he allows his body to awoken 
+        And she allows the seduction 
+                    To betoken 
+              This reproduction 
+                Into an open 
+                  Abduction 
+        Breathless as they have broken 
+        Down the construction 
+        Of a wall that was a token 
+        Of the hurt and destruction 
+                Of the unbroken 
+        they stare at the potion  
+        Of a new love that wasn’t spoken
+                    But a production 
+          Of a love, breathless and open.   
+          Love you 🦋💜`
     },
     {
-      title: 'CrisisRe Veritas',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16"
+      title: 'Smile',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      sample: `
+        Smile 
+        The morning arose upon my room 
+        A phone buzz jerks me from my sleep 
+        For my mind resorts to whom 
+        Only one who can keep 
+        A smile that will bloom 
+        When I open my phone and peep 
+        That my lover has consumed 
+        My phone with messages that steep 
+        Into my heart and resume 
+        To make my heart leap 
+        And groom 
+        My love for him. 💜🦋`
     },
     {
-      title: 'Dragon',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16"
+      title: 'Touch',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      sample: `
+      Your touch 
+      Your embrace 
+      Your eyes to which I race 
+      Your smell to which I chase 
+      I crave you beyond the base 
+      Of life itself for which I face 
+      I love you `
     },
     {
-      title: 'Comnplete',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16"
+      title: 'Rings',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      sample: `
+          Two black rings \n
+          Hold so much weight 
+          For they are not just things 
+          They create 
+          This safe space that brings 
+          A comfort and a gate 
+          To this door that flings 
+          Out this great 
+          Vulnerability that has wings 
+          So vast and allows fate 
+          To sting 
+          And it becomes late 
+          As he hands her his black ring 
+          That hold so much weight. `
     }
  
   ];
   NFT = [
     {
-      title: 'Nightmare Flame',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FA2CB3D73-7042-44EB-8076-0F22AD72A934.jpeg?alt=media&token=b57bc02a-fcea-46ed-be73-cc1f64a65db6"
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
     },
     {
-      title: 'Crisis Lore',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FE499D577-C397-4BAD-BABC-B2FAE698F08E.jpeg?alt=media&token=8e0a4d85-4768-40a4-8079-03c269a52cef"
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
     },
     {
-      title: 'Familia o',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F83D7885A-9D9C-4D02-8257-8B38744FA835.jpeg?alt=media&token=71e3b24f-46a9-4817-a186-a3e983e656d5"
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
     },
     {
-      title: 'Flight',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FCCF733DE-0488-49E1-B128-95350491A843.jpeg?alt=media&token=ad602b29-0866-4796-9d3d-bc66ac99c262"
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
     },
     {
-      title: 'Dias Rojo',
-      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_4533.jpg?alt=media&token=778bd24d-85d3-4fa9-afbd-746ca36b545b"
-    }
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
+    },
+    {
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
+    },
+    {
+      title: 'Img',
+      image: "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/RuiLogo.png?alt=media&token=2a00ea98-57ac-45ee-9da9-008127c19d2a"
+    },
+
  
   ];
 
