62570f8b765b87b46636b4dd808c3482d1b22f1e
Lord muriel <lordmuriel@mbp14inch2021.myfiosgateway.com>
2021-11-30 06:52:20 -0500

    updating banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,36 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //Banner images
+  imgLeft:String = "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FCCF733DE-0488-49E1-B128-95350491A843.jpeg?alt=media&token=ad602b29-0866-4796-9d3d-bc66ac99c262";
+  imgCenter:String ="https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0132.jpg?alt=media&token=7f967075-407b-47fe-b717-8289fbefae05";
+  imgRight:String = "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_4533.jpg?alt=media&token=778bd24d-85d3-4fa9-afbd-746ca36b545b";
+
+  //For pool of images to select from 
+  imgPool: String[] = [
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0078.jpg?alt=media&token=1fb2d9e0-e16c-4bd7-a0d4-338914644a95",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_4533.jpg?alt=media&token=778bd24d-85d3-4fa9-afbd-746ca36b545b",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0132.jpg?alt=media&token=7f967075-407b-47fe-b717-8289fbefae05",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F139B40DF-605B-413E-856A-779E7DB486D2.png?alt=media&token=3ab9687a-fc4e-49b4-b17a-15c9e98d6ea9",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F783E2489-C126-4C86-8638-E7358B037EBF.jpeg?alt=media&token=3476c3a3-6672-414f-a889-6d02c7bbcc09",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FA0F36188-5CF2-4363-A318-47738A38CB7B.jpeg?alt=media&token=2f806254-6039-4a37-a1f3-177f908c69e5",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FE499D577-C397-4BAD-BABC-B2FAE698F08E.jpeg?alt=media&token=8e0a4d85-4768-40a4-8079-03c269a52cef",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FFDE729A0-B3A3-4A91-B343-DE162DFE5AF3.jpeg?alt=media&token=81b11ded-0cc2-48a2-bcc1-ea5acd11f63c",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FC15A5317-C143-4303-BD42-4899D48AB54F.jpeg?alt=media&token=8b0dca04-f269-48ef-8756-17997ec2235d",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FGTpmGkBx6iQWfgmjn9jxkKYBkPn1%2Fimages%2FBE254897-317C-40F9-8EB5-3E6F7F8B8D67.jpeg?alt=media&token=202ddbf9-a7b1-43a2-bbd7-e756197b00a5",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FUntitled_Artwork%2035.png?alt=media&token=64599dbc-a4d7-4dad-a980-336d7c09a077",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F2b67925f-7dff-4070-8ded-15665d3a7ca9.png?alt=media&token=e746ac03-0f38-402d-845c-a9a75846cfac",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F83D7885A-9D9C-4D02-8257-8B38744FA835.jpeg?alt=media&token=71e3b24f-46a9-4817-a186-a3e983e656d5",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FGTpmGkBx6iQWfgmjn9jxkKYBkPn1%2Fimages%2FB6065E30-1353-4F39-96DC-17E32AC92BB0.jpeg?alt=media&token=bee25e36-c348-4b6b-b6dd-c83ac69db2bc",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FCCF733DE-0488-49E1-B128-95350491A843.jpeg?alt=media&token=ad602b29-0866-4796-9d3d-bc66ac99c262",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FB1BF8558-3D05-46E1-830B-627CD82B5A78.jpeg?alt=media&token=a286c12d-5f26-4f71-a720-c83f3a55ad40",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FA%26A.png?alt=media&token=68e4c8e2-a92d-47ba-ae99-090daa4303f5",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FD27BD2CD-1334-4F27-AA66-DCCA45A93166_1_105_c.jpeg?alt=media&token=317823c2-e15d-4b3b-b6e9-293b0871028f",
+    "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FΕλεύθερη%20σκέψη.PNG?alt=media&token=8eeab48b-e3d0-42fd-bff8-d0f06482e96f"
+  ];
+
+
   //Test for array
   testList = [
     {
@@ -232,11 +262,50 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    setInterval(this.shuffleImg, 7000);
   }
+
+
+ 
 
   shuffleImg(){
     //shuffle images 
+    var index =  Math.floor(Math.random() * 3);
+    var index2 = Math.floor(Math.random() * 3);
+    var index3 = Math.floor(Math.random() * 3);
+
+    //Array for images
+    var imgPool = [
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0078.jpg?alt=media&token=1fb2d9e0-e16c-4bd7-a0d4-338914644a95",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_4533.jpg?alt=media&token=778bd24d-85d3-4fa9-afbd-746ca36b545b",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0132.jpg?alt=media&token=7f967075-407b-47fe-b717-8289fbefae05",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FIMG_0140.PNG?alt=media&token=3c72eed7-b2f1-4be7-a20a-cc7b1e7a0a16",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F139B40DF-605B-413E-856A-779E7DB486D2.png?alt=media&token=3ab9687a-fc4e-49b4-b17a-15c9e98d6ea9",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F783E2489-C126-4C86-8638-E7358B037EBF.jpeg?alt=media&token=3476c3a3-6672-414f-a889-6d02c7bbcc09",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FA0F36188-5CF2-4363-A318-47738A38CB7B.jpeg?alt=media&token=2f806254-6039-4a37-a1f3-177f908c69e5",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FE499D577-C397-4BAD-BABC-B2FAE698F08E.jpeg?alt=media&token=8e0a4d85-4768-40a4-8079-03c269a52cef",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FFDE729A0-B3A3-4A91-B343-DE162DFE5AF3.jpeg?alt=media&token=81b11ded-0cc2-48a2-bcc1-ea5acd11f63c",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FC15A5317-C143-4303-BD42-4899D48AB54F.jpeg?alt=media&token=8b0dca04-f269-48ef-8756-17997ec2235d",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FGTpmGkBx6iQWfgmjn9jxkKYBkPn1%2Fimages%2FBE254897-317C-40F9-8EB5-3E6F7F8B8D67.jpeg?alt=media&token=202ddbf9-a7b1-43a2-bbd7-e756197b00a5",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FUntitled_Artwork%2035.png?alt=media&token=64599dbc-a4d7-4dad-a980-336d7c09a077",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F2b67925f-7dff-4070-8ded-15665d3a7ca9.png?alt=media&token=e746ac03-0f38-402d-845c-a9a75846cfac",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2F83D7885A-9D9C-4D02-8257-8B38744FA835.jpeg?alt=media&token=71e3b24f-46a9-4817-a186-a3e983e656d5",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FGTpmGkBx6iQWfgmjn9jxkKYBkPn1%2Fimages%2FB6065E30-1353-4F39-96DC-17E32AC92BB0.jpeg?alt=media&token=bee25e36-c348-4b6b-b6dd-c83ac69db2bc",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FCCF733DE-0488-49E1-B128-95350491A843.jpeg?alt=media&token=ad602b29-0866-4796-9d3d-bc66ac99c262",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FB1BF8558-3D05-46E1-830B-627CD82B5A78.jpeg?alt=media&token=a286c12d-5f26-4f71-a720-c83f3a55ad40",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FA%26A.png?alt=media&token=68e4c8e2-a92d-47ba-ae99-090daa4303f5",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FD27BD2CD-1334-4F27-AA66-DCCA45A93166_1_105_c.jpeg?alt=media&token=317823c2-e15d-4b3b-b6e9-293b0871028f",
+      "https://firebasestorage.googleapis.com/v0/b/obsidianrune-vuejs.appspot.com/o/N4Posts%2FItVzlZqqYXQukCYVI3TZodI7oQq2%2Fimages%2FΕλεύθερη%20σκέψη.PNG?alt=media&token=8eeab48b-e3d0-42fd-bff8-d0f06482e96f"
+    ];
+
+
+    //Update the images every so often 
+    console.log("Updating")
+    this.imgLeft = imgPool[index];
+    this.imgCenter = imgPool[index2];
+    this.imgRight = imgPool[index3];
+    console.log("Finished Updating")
+    
   }
 
 }
