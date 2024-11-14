79b1174ada958b86da66c19fc70488c3d2ea2501
Atakoramuriel <66688682+Atakoramuriel@users.noreply.github.com>
2024-11-14 01:53:40 -0500

    preproduction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBT9KgVgHpDyJt_-11MNEKo_2wJb8jnqfg",
+    authDomain: "obsidianrune-vuejs.firebaseapp.com",
+    databaseURL: "https://obsidianrune-vuejs.firebaseio.com",
+    projectId: "obsidianrune-vuejs",
+    storageBucket: "obsidianrune-vuejs.appspot.com",
+    messagingSenderId: "588393651282",
+    appId: "1:588393651282:web:84be2d26360266176f538b"
+  }
 };
